555588d9ff10633b7afcc313a7bb09b9bcb7a6e1
Bach <borislukovic@outlook.com>
2016-12-28 00:05:13 +0100

    Displaying posts in admin
    Displaying post values from database on admin page

--- admin/posts.php
+++ admin/posts.php
@@ -6,12 +6,9 @@
  <!-- Navigation -->
  <?php include "includes/admin_navigation.php"; ?>
         
- 
 
     <div id="page-wrapper">
-
          <div class="container-fluid">
-
             <!-- Page Heading -->
              <div class="row">
                 <div class="col-lg-12">
@@ -35,27 +32,40 @@
                            </tr>
                        </thead>
                        <tbody>
-                           <tr>
-                               <td>1</td>
-                               <td>Admin</td>
-                               <td>Bootstrap framework</td>
-                               <td>Bootstrap</td>
-                               <td>Status</td>
-                               <td>Image</td>
-                               <td>Tags</td>
-                               <td>Comments</td>
-                               <td>Date</td>
-                           </tr>
+                              <?php
+                                global $connection;
+                                $query = "SELECT * FROM posts";
+                                $select_posts = mysqli_query($connection, $query);
+                           
+                                while($row = mysqli_fetch_assoc($select_posts)) {
+                                    $post_id = $row["post_id"];
+                                    $post_author = $row["post_author"];
+                                    $post_title = $row["post_title"];
+                                    $post_category = $row["post_category_id"];;
+                                    $post_status = $row["post_status"];;
+                                    $post_image = $row["post_image"];;
+                                    $post_tags = $row["post_tags"];;
+                                    $post_comments = $row["post_comment_count"];;
+                                    $post_date = $row["post_date"];;
+                                
+                                echo "<tr>";
+                                echo "<td>{$post_id}</td>"; 
+                                echo "<td>{$post_author}</td>";
+                                echo "<td>{$post_title}</td>";
+                                echo "<td>{$post_category}</td>";
+                                echo "<td>{$post_status}</td>";
+                                echo "<td><image width='80px' height='40' src='../images/$post_image' alt='image'></td>";
+                                echo "<td>{$post_tags}</td>";
+                                echo "<td>{$post_comments}</td>";
+                                echo "<td>{$post_date}</td>";
+                                echo "</tr>";
+                                } 
+                              ?>
                        </tbody>
-                   </table>
-                 
-                   
+                   </table> 
                 </div>
             </div> <!-- /.row -->
-
         </div> <!-- /.container-fluid -->
-
     </div> <!-- /#page-wrapper -->
-
 
 <?php include "includes/admin_footer.php" ?>  <!-- Footer -->
