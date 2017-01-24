d02048f8827aad91acc7df41bbbf3abf9e5e95a0
Bach <borislukovic@outlook.com>
2017-01-24 05:57:21 +0100

    Updating posts
    fixing the images on update

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -4,20 +4,57 @@
                                     
                                 }
                                 
-                                $query = "SELECT * FROM posts ";
+                                $query = "SELECT * FROM posts WHERE post_id = $the_post_id";
                                 $select_posts_by_id = mysqli_query($connection, $query);
                            
                                 while($row = mysqli_fetch_assoc($select_posts_by_id)) {
                                     $post_id = $row["post_id"];
-                                    $post_author = $row["post_author"];
-                                    $post_title = $row["post_title"];
-                                    $post_category = $row["post_category_id"];
-                                    $post_status = $row["post_status"];
-                                    $post_image = $row["post_image"];
-                                    $post_content = $row["post_content"];
-                                    $post_tags = $row["post_tags"];
-                                    $post_comments = $row["post_comment_count"];
-                                    $post_date = $row["post_date"];
+                                    $post_author = $row['post_author'];
+                                    $post_title = $row['post_title'];
+                                    $post_category = $row['post_category_id'];
+                                    $post_status = $row['post_status'];
+                                    $post_image = $row['post_image'];
+                                    $post_content = $row['post_content'];
+                                    $post_tags = $row['post_tags'];
+                                    $post_comments = $row['post_comment_count'];
+                                    $post_date = $row['post_date'];
+                                }
+
+                                if(isset($_POST['update_post'])) {
+                                    $post_author = $_POST['post_author'];
+                                    $post_title = $_POST['post_title'];
+                                    $post_category_id = $_POST['post_category'];
+                                    $post_status = $_POST['post_status'];
+                                    $post_image = $_FILES['image']['name'];
+                                    $post_image_temp = $_FILES['image']['tmp_name'];
+                                    $post_content = $_POST['post_content'];
+                                    $post_tags = $_POST['post_tags'];
+                                    
+                                    //Moving image from the temp location to the permanent location
+                                    move_uploaded_file($post_image_temp, "../images/$post_image");
+                                    if(empty($post_image)) {
+                                        $query = "SELECT * FROM posts WHERE post_id = $the_post_id ";
+                                        $select_image = mysqli_query($connection,$query);
+                                        while($row = mysqli_fetch_array($select_image)) {
+                                            $post_image = $row['post_image'];
+                                        }
+                                    }
+                                    
+                                $query = "UPDATE posts SET ";
+                                $query .="post_title = '$post_title', ";
+                                $query .="post_category_id = '$post_category_id', ";
+                                $query .="post_date = now(), ";
+                                $query .="post_author = '$post_author', ";
+                                $query .="post_status = '$post_status', ";
+                                $query .="post_tags = '$post_tags', ";
+                                $query .="post_content = '$post_content', ";
+                                $query .="post_image = '$post_image' ";
+                                $query .="WHERE post_id = $the_post_id ";
+                                
+                                $update_post = mysqli_query($connection,$query);
+                                
+                                confirmQuery($update_post);
+                                    
                                 }
 ?>
    
@@ -25,11 +62,11 @@
    <form action="" method="post" enctype="multipart/form-data">  <!-- enytype atribut koristimo zbog dodavanja slike -->
     <div class="form-group">
         <label for="title">Post Title</label>
-        <input value="<?php echo $post_title; ?>" type="text" class="form-control" name="title">
+        <input value="<?php echo $post_title; ?>" type="text" class="form-control" name="post_title">
     </div>
     
     <div class="form-group">
-        <select name="" id="">
+        <select name="post_category" id="">
             <?php
                 $query = "SELECT * FROM categories";
                 $select_categories = mysqli_query($connection, $query); 
@@ -48,7 +85,7 @@
    
     <div class="form-group">
         <label for="title">Post Author</label>
-        <input value="<?php echo $post_author; ?>" type="text" class="form-control" name="author">
+        <input value="<?php echo $post_author; ?>" type="text" class="form-control" name="post_author">
     </div>
     
     <div class="form-group">
@@ -57,7 +94,8 @@
     </div>
    
     <div class="form-group">
-        <img width="100" src="..//images/<?php echo $post_image; ?>" alt="">
+        <img width="100" src="../images/<?php echo $post_image; ?>" alt="">
+        <input type="file" name="image">
     </div>
     
     <div class="form-group">
@@ -67,10 +105,10 @@
     
     <div class="form-group">
        <label for="post_category">Post Content</label>
-        <textarea value="<?php echo $post_author; ?>" class="form-control" name="post_content" id="" cols="30" rows="10"> <?php echo $post_content; ?> </textarea>
+        <textarea class="form-control" name="post_content" id="" cols="30" rows="10"><?php echo $post_content; ?> </textarea>
    </div>
    
    <div class="form-group">
-       <input class="btn btn-primary" type="submit" name="create_post" value="Publish Post">
+       <input class="btn btn-primary" type="submit" name="update_post" value="Update Post">
    </div>
 </form>
